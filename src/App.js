d28f8c0b32cf4aade051bae8e732f4c309c12edc
T-MADHAVARAO <mmadboy051@gmail.com>
2023-12-27 16:21:57 +0000

    through this commit added my basic bio data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "./components/HomePage";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Madhav</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={HomePage} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/HomePage/index.js
@@ -0,0 +1,28 @@
+import "./index.css";
+
+const HomePage = () => (
+  <div className="App">
+    <h1 className="details">MY ABOUT </h1>
+    <div className="data">
+      <h1 className="item">
+        Name : <span>TARRA MADHAVA RAO</span>
+      </h1>
+      <h1 className="item">
+        Graduation PassOut Year: <span>JULY-2024</span>
+      </h1>
+      <h1 className="item">Technologies/ Language Used: </h1>
+      <ul className="technologies">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>BootStrap</li>
+        <li>JavaScript</li>
+        <li>ReactJS</li>
+        <li>Python</li>
+        <li>SQLite</li>
+        <li>Node.JS (Express.js)</li>
+      </ul>
+    </div>
+  </div>
+);
+
+export default HomePage;
